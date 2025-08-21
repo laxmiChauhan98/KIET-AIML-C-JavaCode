8bfa3ee506cda50957b050eb72b1605e5ff225d6
laxmiChauhan98 <laxchauhan98@gmail.com>
2025-08-21 16:22:56 +0530

    21-08-2025

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld{
     public static void main(String[] args) {
         System.out.println("Hello KIET");
+        System.out.println("This is a program to welcome you in Java");
     }
 }

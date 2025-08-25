d51c86cfc32ce28ea56d4f3558f9ec98e458966c
laxmiChauhan98 <laxchauhan98@gmail.com>
2025-08-25 13:32:19 +0530

    Added

---
+++ EncapsuLation.java
@@ -0,0 +1,38 @@
+class EmployeeEncap{
+    private int salary;            //Data hiding
+    private String password="KIET@123";
+
+    public void setSalary(int salary, String pswd){
+        if(pswd.equals(password)){
+
+        this.salary=salary;
+
+        System.out.println("Salary is credited succesfully");
+        }
+        else{
+            System.out.println("No Info Available");
+        }
+    }
+
+    public int getSalary(String pswd){
+        if(pswd.equals(password)){
+            System.out.println("Amount is: "+salary);
+             return salary;
+        }
+        else{
+            System.out.println("Wrong Password: Access Denied");
+        }
+        return -1;
+       
+    }
+}
+public class EncapsuLation{
+    public static void main(String[] args) {
+        
+    EmployeeEncap ee=new EmployeeEncap();
+    ee.setSalary(10000000, "KIET@123");
+    ee.getSalary("KIET@123");
+
+    }
+    
+}
---
+++ PolyMorphism.java
@@ -0,0 +1,42 @@
+class EmployeePoly{
+    String Emp_name;
+    
+
+    void Emp_details(){
+        System.out.println("Name of the Employee is: "+Emp_name);
+    }
+
+    //Method overloading or Compile time Polymorphism
+
+    void Emp_details(int salary){
+        System.out.println("Salary of the Employee is: " +salary);
+    }    
+
+    void Emp_details(int salary, int raise){
+        System.out.println("Salary after appraisal is: " +(salary+raise));
+
+    }
+}
+class Faculties extends EmployeePoly{
+
+    void Emp_details(){
+        super.Emp_details();
+        System.out.println("Welcome "+Emp_name);
+    }
+
+}
+public class PolyMorphism {
+      public static void main(String[] args) {
+        EmployeePoly emp=new EmployeePoly();
+        emp.Emp_name="Ayushi";
+        emp.Emp_details();
+
+        emp.Emp_details(300000);
+
+        emp.Emp_details(300000, 20000);
+
+        Faculties f=new Faculties();
+        f.Emp_name="Shikha Chauhan";
+        f.Emp_details(); 
+}
+}
